f986baf8ffca8af1fb8b34abf2a1eb28546cd609
William Stein <williamstein92@gmail.com>
2014-07-15 10:26:20 -0400

    colors, fonts

---
+++ app/tests.js
@@ -0,0 +1,108 @@
+(function () {
+  var TESTS;
+
+  TESTS = {
+    "icons": {
+      "call": {
+        "element": "i",
+        "class": "i-call"
+      },
+      "code": {
+        "element": "i",
+        "class": "i-code"
+      },
+      "delete": {
+        "element": "i",
+        "class": "i-delete"
+      },
+      "down-arrow": {
+        "element": "i",
+        "class": "i-down-arrow"
+      },
+      "edit": {
+        "element": "i",
+        "class": "i-edit"
+      },
+      "facebook": {
+        "element": "i",
+        "class": "i-facebook"
+      },
+      "flashlight": {
+        "element": "i",
+        "class": "i-flashlight"
+      },
+      "google-plus": {
+        "element": "i",
+        "class": "i-google-plus"
+      },
+      "instagram": {
+        "element": "i",
+        "class": "i-instagram"
+      },
+      "location": {
+        "element": "i",
+        "class": "i-location"
+      },
+      "mail": {
+        "element": "i",
+        "class": "i-mail"
+      },
+      "menu": {
+        "element": "i",
+        "class": "i-menu"
+      },
+      "photo": {
+        "element": "i",
+        "class": "i-photo"
+      },
+      "pinterest": {
+        "element": "i",
+        "class": "i-pinterest"
+      },
+      "reload": {
+        "element": "i",
+        "class": "i-reload"
+      },
+      "rss": {
+        "element": "i",
+        "class": "i-rss"
+      },
+      "search": {
+        "element": "i",
+        "class": "i-search"
+      },
+      "share": {
+        "element": "i",
+        "class": "i-share"
+      },
+      "twitter": {
+        "element": "i",
+        "class": "i-twitter"
+      },
+      "video": {
+        "element": "i",
+        "class": "i-video"
+      },
+      "view": {
+        "element": "i",
+        "class": "i-view"
+      },
+      "warning": {
+        "element": "i",
+        "class": "i-warning"
+      },
+      "x": {
+        "element": "i"
+        "class": "i-x"
+      },
+      "youvisit": {
+        "element": "i"
+        "class": "i-youvisit"
+      }
+    },
+    "common": {
+
+    }
+  };
+
+})();
