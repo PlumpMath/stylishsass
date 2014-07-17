24bfa0f929bb5ab2158714063f71cb020e3e8f90
William Stein <williamstein92@gmail.com>
2014-07-17 13:12:46 -0400

    font utilities & icon fonts

--- index.php
+++ index.php
@@ -185,6 +185,335 @@ $lists = [
       ],
     ]
   ],
+  [
+    'name' => 'Icons',
+    'items' => [
+      [
+        'name' => 'call',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-call'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'code',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-code'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'delete',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-delete'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'down-arrow',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-down-arrow'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'edit',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-edit'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'facebook',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-facebook'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'flashlight',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-flashlight'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'google-plus',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-google-plus'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'heart',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-heart'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'instagram',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-instagram'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'location',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-location'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'mail',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-mail'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'media',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-media'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'menu',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-menu'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'mobile',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-mobile'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'people',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-people'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'photo',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-photo'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'pinterest',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-pinterest'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'reload',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-reload'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'rss',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-rss'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'search',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-search'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'share',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-share'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'twitter',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-twitter'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'video',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-video'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'view',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-view'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'x',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-x'
+            ]
+          ],
+        ]
+      ],
+      [
+        'name' => 'yield',
+        'items' => [
+          [
+            'name' => 'default',
+            'file' => 'icon',
+            'items' => [
+              'class' => 'icon-yield'
+            ]
+          ],
+        ]
+      ],
+    ]
+  ],
 ];
 
 ?>
